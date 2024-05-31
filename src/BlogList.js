6795a084f91971233cd9e90b4cc23c5994d76891
Zubiyan <shaikhmohdzubiyan@gmail.com>
2024-05-31 16:39:22 -0400

    first commit

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,19 +1,31 @@
-// src/BlogList.js
 import React from 'react';
 import BlogPost from './BlogPost';
 import './BlogList.css';
 
 const BlogList = () => {
   const posts = [
-    { title: 'First Post', content: 'This is the content of the first post.' },
-    { title: 'Second Post', content: 'This is the content of the second post.' },
+    { 
+      title: 'First Post',
+      content: 'This is the content of the first post.',
+      image: 'react.png'
+    },
+    { 
+      title: 'Second Post',
+      content: 'This is the content of the second post.',
+      image: 'angular.png'
+    },
     // Add more posts here
   ];
 
   return (
     <div className="blog-list">
       {posts.map((post, index) => (
-        <BlogPost key={index} title={post.title} content={post.content} />
+        <BlogPost 
+          key={index} 
+          title={post.title} 
+          content={post.content} 
+          imageUrl={post.imageUrl} // Pass imageUrl as a prop
+        />
       ))}
     </div>
   );
--- src/BlogPost.js
+++ src/BlogPost.js
@@ -5,6 +5,7 @@ import './BlogPost.css';
 const BlogPost = ({ title, content }) => {
   return (
     <div className="blog-post">
+      <img src="react.png"></img>
       <h2>{title}</h2>
       <p>{content}</p>
     </div>
